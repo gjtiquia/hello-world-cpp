f2a48b1d73a9c3654d0f7ede28f7574530898fc0
GJ Tiquia <gershom.tiquia@gmail.com>
2024-12-02 18:29:07 -0500

    treat compile warnings as errors
    Halt compilation if finds any warnings

--- build.sh
+++ build.sh
@@ -5,14 +5,19 @@ echo "Build: Compiling main.cpp into executable 'main'..."
 # Minimalistic version
 # g++ main.cpp -o main
 
-# Add -ggdb for compiling a debug build for use wuth gdb
+# Add -ggdb before the filenamefor compiling a debug build for use with gdb
 # (source: https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-build-configurations/)
 # g++ -ggdb main.cpp -o main
 # then run: "gdb ./main"
 # However, personally i think a debugger works better tgt with an IDE, especially for breakpoints etc.
 
-# Increase warning level with these flags: -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion
+# Increase warning level with these flags before the filename: -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion
 # (source: https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-warning-and-error-levels/)
-g++ -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion main.cpp -o main
+# g++ -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion main.cpp -o main
+
+# Treat warnings as errors with the following flag before the filename: -Werror
+# The compiler will halt compilation if it finds any warnings
+# (source: https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-warning-and-error-levels/)
+g++ -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Werror main.cpp -o main
 
 echo "Build Success!"
