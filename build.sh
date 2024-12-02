fccbbfe79664254f0b6e475167d372b18cc0e633
GJ Tiquia <gershom.tiquia@gmail.com>
2024-12-02 16:53:51 -0500

    Update build.sh with higher warning levels

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash 
 
 echo "Build: Compiling main.cpp into executable 'main'..."
-g++ main.cpp -o main
+
+# Minimalistic version
+# g++ main.cpp -o main
+
+# Add -ggdb for compiling a debug build for use wuth gdb
+# (source: https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-build-configurations/)
+# g++ -ggdb main.cpp -o main
+# then run: "gdb ./main"
+# However, personally i think a debugger works better tgt with an IDE, especially for breakpoints etc.
+
+# Increase warning level with these flags: -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion
+# (source: https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-warning-and-error-levels/)
+g++ -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion main.cpp -o main
 
 echo "Build Success!"
